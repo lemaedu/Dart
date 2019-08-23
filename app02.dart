022b4bf9c072ae5cc3f3644aa4f203d48c9b9059
lemaedu <lemaedu@gmail.com>
2019-08-23 18:06:32 -0500

    Update app02.dart

--- app02.dart
+++ app02.dart
@@ -1,10 +1,13 @@
-//Tipos de datos Numero y String
 void main() {
 
-//Numero
+//Numero ---------------------------------
   int a=10;
   //En un double simepre hay que indicar la presicion,
   //caso contrario mas adelante dart lo puede confundir con un int
   double pi=3.1415;
   print('Edad $a - $pi');
+//String----------------------------------
+  String nombre='lemaedu';
+  print('Nombre: $nombre');
+  
 }
