2400e3b336dfa55c4f04501cf9614ec7b57fb4ac
lemaedu <lemaedu@gmail.com>
2019-08-23 18:11:09 -0500

    Update app02.dart

--- app02.dart
+++ app02.dart
@@ -2,8 +2,7 @@ void main() {
 
 //Numero ---------------------------------
   int a=10;
-  //En un double simepre hay que indicar la presicion,
-  //caso contrario mas adelante dart lo puede confundir con un int
+  //En un double simpre se debe indicar la presicion, caso contrario dart  puede interpretar como un int
   double pi=3.1415;
   print('Edad $a - $pi');
   //String----------------------------------
