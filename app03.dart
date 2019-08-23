aadd59e64ea469d49592c2c341703548fb415222
lemaedu <lemaedu@gmail.com>
2019-08-23 18:28:18 -0500

    Create app03.dart

---
+++ app03.dart
@@ -0,0 +1,15 @@
+void main() {
+
+//bolean, son importantes para trabajar con condiciones
+  bool activado=true;
+  
+  activado=!activado;
+  
+  if(activado){
+    print('el Motor esta prendido');
+  }
+  else{
+    print('el Motor esta apagado');
+  }
+  
+}
